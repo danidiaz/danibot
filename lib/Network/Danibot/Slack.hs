f18f9cdca2882521852acbcb57d872c414baf255
ddc <diaz_carrete@yahoo.com>
2016-03-01 00:13:13 +0100

    Star.

--- lib/Network/Danibot/Slack.hs
+++ lib/Network/Danibot/Slack.hs
@@ -4,15 +4,19 @@ module Network.Danibot.Slack (isDirectedTo,mute) where
 
 import Data.Text (Text)
 import Data.Char
+import Data.Profunctor (Star(..))
 import qualified Data.Attoparsec.Text as Atto
 
 import Control.Applicative
 import Control.Concurrent.MVar
 
-import Network.Danibot.Slack.Types (UserMessage(..))
+import Network.Danibot.Slack.Types (Event,UserMessage(..))
 
 mute :: IO a
 mute = newEmptyMVar >>= takeMVar
+
+handlers :: [Event -> IO ()] -> Event -> IO () 
+handlers = runStar . foldr (*>) (pure ()) . map Star
 
 isDirectedTo :: Text -> Maybe (Text,Text)
 isDirectedTo txt = case Atto.parse mentionParser txt of
