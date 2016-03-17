d0e2cbbb7e22999df6ddfb31747a5491f18cd327
ddc <diaz_carrete@yahoo.com>
2016-03-17 23:42:30 +0100

    isUp now works

--- lib/Network/Danibot.hs
+++ lib/Network/Danibot.hs
@@ -11,8 +11,11 @@ import Data.Monoid
 import Data.Char
 import Data.Text (Text)
 import qualified Data.Text as Text
+import qualified Data.Attoparsec.Text as Atto
 import Text.Read
+import Control.Applicative
 import Control.Concurrent
+import Control.Concurrent.Async
 import Network
 import System.IO
 import Control.Exception
@@ -20,15 +23,34 @@ import Control.Exception
 dumbHandler :: Text -> IO Text
 dumbHandler _ = do 
     threadDelay 0.5e6
-    return "I don't do anything yet."
+    return "Nothing was done."
 
 isUpHandler :: Text -> IO Text
-isUpHandler (Text.break isSpace . Text.strip -> (Text.unpack . Text.strip -> host,Text.unpack . Text.strip -> port)) = do 
-    case readMaybe port of
-        Just numericport ->
-                (catch :: IO a -> (SomeException -> IO a) -> IO a)
-                (bracket (Network.connectTo host (PortNumber (fromInteger numericport)))
-                         hClose
-                         (\_ -> pure "The port is listening."))
-                (\e -> pure ("Exception when trying to connect: " <> Text.pack (show e)))
+isUpHandler (Text.break isSpace . Text.strip -> (Text.strip -> host,Text.strip -> port)) = do 
+    print host
+    print (extractAddress host)
+    case (extractAddress host, readMaybe (Text.unpack port)) of
+        (Right host', Just numericport) -> do
+            let attempt =
+                    catchAny
+                    (bracket (Network.connectTo (Text.unpack host') (PortNumber (fromInteger numericport)))
+                             hClose
+                             (\_ -> pure "The port is listening."))
+                    (\e -> pure ("Exception when trying to connect: " <> Text.pack (show e)))
+            either id id <$> race attempt (threadDelay 3e6 *> pure "Timeout.")
         _ -> pure "Expected arguments: host port"
+    where
+    catchAny :: IO a -> (SomeException -> IO a) -> IO a
+    catchAny = catch
+
+    extractAddress :: Text -> Either String Text
+    extractAddress txt = Atto.parseOnly extractor txt
+        where
+        extractor = 
+            (Atto.string "<" *> Atto.takeTill (\c -> c == '|') 
+                             *> Atto.string   "|"
+                             *> Atto.takeTill (\c -> c == '>') 
+                             <* Atto.string ">")
+            <|>
+            Atto.takeText
+
