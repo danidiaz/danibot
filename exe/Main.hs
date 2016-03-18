7e19be276c7ac4a1c071aaee34e6b039959752d6
ddc <diaz_carrete@yahoo.com>
2016-03-19 00:11:23 +0100

    dispatch

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,14 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (
         main
     ) where
 
-import Network.Danibot (dumbHandler,isUpHandler)
+import Data.Monoid
+import Network.Danibot
 import Network.Danibot.Main (mainWith)
 
 import Options.Applicative 
 import qualified Options.Applicative as Options
 
 main :: IO ()
-main = mainWith (pure (Right isUpHandler))
-
+main = mainWith (pure (Right (dispatch handlersWithHelp)))
+    where
+    handlers = [("nop",dumbHandler),("up?",isUpHandler),("lookup",lookupHandler mempty)]
+    handlersWithHelp = [("help", helpHandler handlers)] ++ handlers 
 
--- lib/Network/Danibot.hs
+++ lib/Network/Danibot.hs
@@ -4,11 +4,17 @@
 
 module Network.Danibot (
        dumbHandler
+    ,  lookupHandler
     ,  isUpHandler
+    ,  helpHandler
+    ,  dispatch
     ) where
 
 import Data.Monoid
 import Data.Char
+import Data.Map
+import Data.List
+import qualified Data.Map as Map
 import Data.Text (Text)
 import qualified Data.Text as Text
 import qualified Data.Attoparsec.Text as Atto
@@ -20,23 +26,32 @@ import Network
 import System.IO
 import Control.Exception
 
-dumbHandler :: Text -> IO Text
-dumbHandler _ = do 
-    threadDelay 0.5e6
-    return "Nothing was done."
+dumbHandler :: (Text -> IO Text,Text)
+dumbHandler = 
+    let go _ = do 
+            threadDelay 0.5e6
+            return "Nothing was done."
+    in  (go,"")
 
-isUpHandler :: Text -> IO Text
-isUpHandler (Text.break isSpace . Text.strip -> (Text.strip -> host,Text.strip -> port)) = do 
-    case (extractAddress host, readMaybe (Text.unpack port)) of
-        (Right host', Just numericport) -> do
-            let attempt =
-                    catchAny
-                    (bracket (Network.connectTo (Text.unpack host') (PortNumber (fromInteger numericport)))
-                             hClose
-                             (\_ -> pure "The port is listening."))
-                    (\e -> pure ("Exception when trying to connect: " <> Text.pack (show e)))
-            either id id <$> race attempt (threadDelay 3e6 *> pure "Timeout.")
-        _ -> pure "Expected arguments: host port"
+lookupHandler :: Map Text Text -> (Text -> IO Text,Text) 
+lookupHandler aMap = 
+    let go key = pure (maybe "Entry not found." id (Map.lookup key aMap))
+    in  (go,"key") 
+
+isUpHandler :: (Text -> IO Text,Text)
+isUpHandler =
+    let go (Text.break isSpace . Text.strip -> (Text.strip -> host,Text.strip -> port)) = do 
+            case (extractAddress host, readMaybe (Text.unpack port)) of
+                (Right host', Just numericport) -> do
+                    let attempt =
+                            catchAny
+                            (bracket (Network.connectTo (Text.unpack host') (PortNumber (fromInteger numericport)))
+                                     hClose
+                                     (\_ -> pure "The port is listening."))
+                            (\e -> pure ("Exception when trying to connect: " <> Text.pack (show e)))
+                    either id id <$> race attempt (threadDelay 3e6 *> pure "Timeout.")
+                _ -> pure "Expected arguments: host port"
+    in  (go, "host port")
     where
     catchAny :: IO a -> (SomeException -> IO a) -> IO a
     catchAny = catch
@@ -52,3 +67,17 @@ isUpHandler (Text.break isSpace . Text.strip -> (Text.strip -> host,Text.strip -
             <|>
             Atto.takeText
 
+helpHandler :: [(Text,(Text -> IO Text,Text))] -> (Text -> IO Text, Text)
+helpHandler handlers = 
+    let helptext = mconcat (intersperse "\n" [k <> " " <> v | (k,(_,v)) <- handlers])
+    in  (\_ -> pure helptext,"")
+
+splitWord :: Text -> (Text,Text)
+splitWord (Text.break isSpace . Text.strip -> (Text.strip -> word,Text.strip -> rest)) =  (word,rest)
+
+dispatch :: [(Text,(Text -> IO Text,Text))] -> Text -> IO Text
+dispatch (Map.fromList -> handlers) (splitWord -> (key,request)) =   
+    case Map.lookup key handlers of
+        Just (f,_) -> f request
+        Nothing    -> pure "Unknown command."
+    
